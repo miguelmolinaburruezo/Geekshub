3a9eb6ea6af35ad40a7a3b306b15bca29fb415b4
miguel molina <miguelmolinaburruezo2@gmail.com>
2018-10-29 14:17:48 +0100

    First commit

---
+++ src/main.js
@@ -0,0 +1,65 @@
+/*
+                console.log(1);
+
+                console.log('1');
+
+                console.log(true);
+
+                // var miVariable;
+                // console.log(miVariable);
+
+
+                console.log({
+                name: 'Miguel',
+                bool: true,
+                defined: undefined,
+                miVariable: miVariable
+                });
+                */
+
+/*
+var miObjeto = {
+    name: 'Miguel',
+    bool: true
+};
+
+console.log(miObjeto);
+console.log(miObjeto.name);
+console.log(miObjeto['bool']);
+*/
+var arr = new Array(1,2,3,'hola');
+var arr2 = [1,2,3,'hola'];
+
+arr[4] ='indice4';
+console.log('Array Indice 3 ->',arr[3]);
+console.log('Array Indice 4 ->',arr[4]);
+console.log(arr2);
+
+console.log(new Date(1000));
+
+var mivar= 1+1;
+
+variable4= prompt('Introduce un valor');
+console.log(parseInt(variable4));
+console.log(variable4);
+console.log('1' + 1);
+
+console.log(2**3)
+console.log(Math.Pow(2,3));
+
+var miVar=2;
+console.log(++miVar);
+mivar++
+console.log(mivar);
+
+console.log(+'2');
+
+console.log(false && true); //false
+console.log(false && false); //false
+console.log(true && true); //true
+console.log(true && false); //false
+
+console.log(false || true);
+console.log(true || false);
+console.log(true || false);
+console.log(true || false);
